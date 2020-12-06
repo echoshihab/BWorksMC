0b637a0f8ccaf1ba0178d123022ad9c1d4c8f32c
shihab <echoshihab@gmail.com>
2020-12-06 00:42:44 -0500

    first commit

---
+++ Domain/BloodWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Domain
+{
+    public class BloodWork
+    {
+        public DateTime DateCreated { get; set; }
+        public DateTime ExamDate { get; set; }
+        public DateTime ResultsDate { get; set; }
+        public decimal Hemoglobin { get; set; }
+        public decimal Hematocrit { get; set; }
+        public decimal WBCellsCount { get; set; }
+        public decimal RBCellsCount { get; set; }
+
+    }
+}
---
+++ Persistence/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : IdentityDbContext<AppUser>
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<BloodWork> BloodWorks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
